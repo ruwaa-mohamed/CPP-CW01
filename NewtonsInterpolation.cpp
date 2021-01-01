c9f2e0f8625ee2d4c1a8e02bf89a3ace450b246c
ruwaa-mohamed <ruwaa.ibrahem@gmail.com>
2021-01-01 17:23:12 +0200

    init

---
+++ NewtonsInterpolation.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Part 3- Newton\'s Interpolation";
+    
+    std::cout << std::endl;
+    return 0;
+}
---
+++ coursework01.mk
@@ -0,0 +1,110 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=coursework01
+ConfigurationName      :=Debug
+WorkspacePath          :=/home/ruwaa/Documents/NU/CIT644-ScientificComputing_Elhelw/CodeLite
+ProjectPath            :=/home/ruwaa/Documents/NU/CIT644-ScientificComputing_Elhelw/CodeLite/coursework01
+IntermediateDirectory  :=./Debug
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=Ruwaa
+Date                   :=01/01/21
+CodeLitePath           :=/home/ruwaa/.codelite
+LinkerName             :=/usr/bin/g++
+SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="coursework01.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=mkdir -p
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/g++
+CC       := /usr/bin/gcc
+CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
+CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := /usr/bin/as
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=/usr/share/codelite
+Objects0=$(IntermediateDirectory)/NewtonsInterpolation.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@test -d ./Debug || $(MakeDirCommand) ./Debug
+
+
+$(IntermediateDirectory)/.d:
+	@test -d ./Debug || $(MakeDirCommand) ./Debug
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/NewtonsInterpolation.cpp$(ObjectSuffix): NewtonsInterpolation.cpp $(IntermediateDirectory)/NewtonsInterpolation.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ruwaa/Documents/NU/CIT644-ScientificComputing_Elhelw/CodeLite/coursework01/NewtonsInterpolation.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NewtonsInterpolation.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/NewtonsInterpolation.cpp$(DependSuffix): NewtonsInterpolation.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/NewtonsInterpolation.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/NewtonsInterpolation.cpp$(DependSuffix) -MM NewtonsInterpolation.cpp
+
+$(IntermediateDirectory)/NewtonsInterpolation.cpp$(PreprocessSuffix): NewtonsInterpolation.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/NewtonsInterpolation.cpp$(PreprocessSuffix) NewtonsInterpolation.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r ./Debug/
+
+
